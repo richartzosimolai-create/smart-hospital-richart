1f5fb1429a32932bd302c6a6902b30e00c4a7bd5
richartzosimolai-create <richartzosimolai@gmail.com>
2026-07-03 13:23:07 +0700

    update

--- hospital_app.py
+++ hospital_app.py
@@ -113,7 +113,7 @@ with st.form("triage_form"):
     <div style="background:#131c29;border:1px solid #cbdc0d;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#25f20a;color:white;border-radius:8px;
+            <span style="background:#c7f20a;color:white;border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">1</span>
             <span style="font-size:16px;font-weight:600;color:#105efa;">What are your main symptoms?</span>
             <span style="font-size:13px;color:#6b7280;font-style:italic;">select all that apply</span>
