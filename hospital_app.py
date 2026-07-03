75546858c15976638cee0ccba0e9c67d4ca7be41
richartzosimolai-create <richartzosimolai@gmail.com>
2026-07-03 13:29:48 +0700

    update

--- hospital_app.py
+++ hospital_app.py
@@ -156,9 +156,9 @@ with st.form("triage_form"):
 
     col_cc, col_dur = st.columns(2)
     with col_cc:
-        chief_complaint = st.selectbox("CODENO15", options=list(cc_map.keys()))
+        chief_complaint = st.selectbox("Chief complaint", options=list(cc_map.keys()))
     with col_dur:
-        duration = st.selectbox("CODENO16", options=list(dur_map.keys()), index=1)
+        duration = st.selectbox("Duration", options=list(dur_map.keys()), index=1)
 
     st.markdown("<br>", unsafe_allow_html=True)
 
